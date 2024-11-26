578941fd5cd7b61981f686dd950648a81dedc786
Eimantasas <152972605+Eimantasas@users.noreply.github.com>
2024-11-26 13:31:25 +0100

    Sparemålskalkulator integrering

--- Sparemkalkulator/script.js
+++ Sparemkalkulator/script.js
@@ -1,21 +1,16 @@
 
 function submitInfo() {
 
-    let interestChecked = document.getElementById("boolinterest").checked;
     let goalValue = parseFloat(document.getElementById("goalamount").value);
     let startValue = parseFloat(document.getElementById("startamount").value);
     let timeValue = parseFloat(document.getElementById("timeframe").value);
-    let interestValue = 0
 
-    if (interestChecked){
-        interestValue = parseFloat(document.getElementById("interest").value);
-    } else {interestChecked = 0}
-    calculate(goalValue, startValue, timeValue, interestValue, interestChecked);
+    calculate(goalValue, startValue, timeValue);
 }
 
 
 //Ikke ferdig med renters rente
-function calculate(goal, start, time, interest, isChecked) {
+function calculate(goal, start, time) {
     if (start > goal){
         displayResult("Vennligst oppgi gyldig informasjon (sparemålet er større enn nåværende besparelser)")
     }
@@ -28,11 +23,7 @@ function calculate(goal, start, time, interest, isChecked) {
     else if (isNaN(time) || time <= 0){
         displayResult("Vennligst oppgi et gyldig antall måneder")}
 
-    else if (isChecked && isNaN(interest) || isChecked && interest <= 0){
-        displayResult("Vennligst oppgi en gyldig rente")
-    }
-
-     else {monthlySavingsNeeded = (goal - start) / time
+    else {monthlySavingsNeeded = (goal - start) / time
         displayResult(`Du må spare ${monthlySavingsNeeded.toFixed(2)} kr hver måned i ${time} måneder`)}
 }
 
